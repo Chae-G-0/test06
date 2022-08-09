9532fcf4ef860f3f514af97de7976f4fb361bd8e
chae <corkdud@naver.com>
2022-08-09 17:50:51 +0900

    220809

--- js/test06.js
+++ js/test06.js
@@ -6,4 +6,33 @@ $(function () {
         pauseOnFocus: false,
         pauseOnHover: false,
     });
+
+    $(".menuSlide").slick({
+        autoplay: false,
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        centerPadding: "450px",
+        sliedToShow: 3,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        swipeToSlide: true,
+    });
+
+    $(".inSlide").slick({
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        centerPadding: "600px",
+        sliedToShow: 3,
+    });
+
+    $(".tabMenu>li>a").on("click", function (e) {
+        e.preventDefault();
+        var idx = $(this).parent().index();
+        $(".tabContent>li").removeClass("on");
+        $(".tabContent>li").eq(idx).addClass("on");
+        $(".tabMenu>li").removeClass("on");
+        $(this).parent().addClass("on");
+    });
 });
